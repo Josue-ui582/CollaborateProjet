82d156d31b0ac33686689e6c4a89eeff64a9f21a
Josue-ui582 <hounhouijosue1@gmail.com>
2024-01-21 01:05:51 +0100

    Corriger Exercice 3.
    Quelques feedback à faire

---
+++ Days2Exo3.js
@@ -0,0 +1,13 @@
+//1
+let originalSentence = 'L\'amour est la meilleure chose au monde. Certains ont trouvé leur amour et d\'autres cherchent toujours  leur amour'
+let wordToCount = 'amour'
+let newSentence = originalSentence.split(wordToCount)
+let count = newSentence.length - 1
+console.log(count)
+//2
+//3
+//4
+let salaire = 5000
+let prime = 10000
+let revenusCours = 15000
+console.log(salaire*12 + prime + revenusCours*12)
